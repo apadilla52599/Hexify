1fdea71beecdfc10fcd1189587e150a79f2e3dbe
Alex <apadilla52599@gmail.com>
2020-10-30 18:10:04 -0400

    Changed Hamburger menu

--- src/HamburgerMenu.js
+++ src/HamburgerMenu.js
@@ -1,4 +1,14 @@
 import React from "react";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import AddCircleIcon from '@material-ui/icons/AddCircle'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/Delete';
 import img1 from "./DummyData/p1.JPG";
 import img2 from "./DummyData/p2.JPG";
 import img3 from "./DummyData/p3.JPG";
@@ -7,6 +17,7 @@ import img5 from "./DummyData/p5.JPG";
 import img6 from "./DummyData/p6.JPG";
 import img7 from "./DummyData/p7.JPG";
 import img8 from "./DummyData/p8.JPG";
+import { Divider } from "@material-ui/core";
 
 class HamburgerMenu extends React.Component {
     constructor(){
@@ -31,20 +42,41 @@ class HamburgerMenu extends React.Component {
     render() {
         return (
             <div id="hamburger_menu" style={{ display: "flex", width: "0%", backgroundColor: "black", visibility:"hidden", position:"relative" }}>
-                <table>
-                    <tbody>
-                    {this.state.graphList.map((graph,index) => (
-                        <tr key={index}>
-                            <td className="imgSpace">
-                                <img className="thumb" alt="graphThumbnail" src={graph.thumbnail}></img>
-                            </td>
-                            <td className="titleSpace">
-                                {graph.name}
-                            </td>
-                        </tr>
-                        ))}
-                    </tbody>
-                </table>
+                 <div>
+                    <List style={{overflowY: "scroll", width: "100%", height: "100%"}}>
+                    <ListItem key="create-new">
+                        <ListItem button={true}>
+                        <ListItemAvatar style={{color: "white"}}>
+                            <AddCircleIcon fontSize="large"/>
+                        </ListItemAvatar>
+                        <ListItemText style={{color: "white"}}>
+                            Create new graph
+                        </ListItemText>
+                        </ListItem>
+                    </ListItem>
+                    <Divider/>
+                    <Typography variant="h6" style={{color:"black", backgroundColor: "blueviolet"}}>
+                        GRAPHICAL PLAYLISTS
+                    </Typography>
+                    <Divider/>
+                    {this.state.graphList.map( (graph, index) => (
+                        <ListItem button={true} key={index}>
+                        <ListItemAvatar>
+                            <Avatar sizes="large" src={graph.thumbnail}></Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={graph.name} style={{color: "white"}}
+                            secondary={graph.dateModified}
+                        />
+                        <ListItemSecondaryAction>
+                            <IconButton edge="end" aria-label="delete">
+                            <DeleteIcon style={{color:"white"}}/>
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                        </ListItem>
+                    ))}
+                    </List>
+                </div>
             </div>
         );
     }
