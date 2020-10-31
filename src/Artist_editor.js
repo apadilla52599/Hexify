8058e812b14d39ddab849d1504b060957aa4cd05
Ethan DeTurk <ethan.deturk@stonybrook.edu>
2020-10-30 23:12:52 -0400

    Fix artist editor CSS units and change button color

--- src/Artist_editor.js
+++ src/Artist_editor.js
@@ -9,28 +9,26 @@ class Artist_editor extends React.Component {
         return (
             <div id="playlist_editor" style = {{align: "center"}}>
                 <div style={{display: "flex", alignItems: "center" }}>
-                    <div  style={{paddingTop: ".5vh", display: "flex", justifyContent: "center", flexGrow: 1}}>
-					    <img style = {{width: "5vw", height: "5vw", borderRadius: "50%"}} src= {artist.imageURL} className="img-responsive" alt=""/>
+                    <div  style={{paddingTop: "1.5rem", display: "flex", justifyContent: "center", flexGrow: 1}}>
+					    <img style = {{width: "6rem", height: "6rem", borderRadius: "50%"}} src= {artist.imageURL} className="img-responsive" alt=""/>
 				    </div>
                 </div>     
 				
-				<div style ={{textAlign: "center", marginTop: ".5vh"}}>
-					<div style = {{color: "B19CD9", fontSize: "1.5vw",fontWeight: "600", marginBottom: ".25vh", fontFamily: "monospace"}}>
+				<div style ={{textAlign: "center", marginTop: ".5rem"}}>
+					<div style = {{color: "B19CD9", fontSize: "1.5rem",fontWeight: "600", marginBottom: ".25rem", fontFamily: "monospace"}}>
 						{artist.name}
 					</div>
-					<div style= {{color: "white",fontSize: "1vw", fontWeight: "200",marginbottom: "1vh", fontFamily: "monospace"}}>
+					<div style= {{color: "white",fontSize: "1rem", fontWeight: "200",marginbottom: "1rem", fontFamily: "monospace"}}>
                         {artist.genres[0]}
 					</div>
 				</div>
 
-                <div  style = {{display: "flex", justifyContent: "center", flexGrow: 1, marginTop: "2vh", marginBottom: "1vh"}}>
-                    <input style = {{height: "2vw", width:"15vw", fontSize: "1vw"}} type="text" placeholder="Search.." ></input>
+                <div  style = {{display: "flex", justifyContent: "center", flexGrow: 1, marginTop: "1rem"}}>
+                    <input style = {{height: "2rem", width: "90%", fontSize: "1rem"}} type="text" placeholder="Search.." ></input>
                 </div>
-                <div style = {{marginBottom: "2vh"}}> 
-                    <ArtistSongList />
-                </div>
-                <div style = {{display: "flex", justifyContent: "center", flexGrow: 1, marginTop: ".5vh"}}>
-                    <Button variant="contained" color="secondary"style = {{width: "10vw", height: "2vw", fontSize: ".75vw"}}>
+                <ArtistSongList />
+                <div style = {{display: "flex", justifyContent: "center", flexGrow: 1, marginTop: "1rem"}}>
+                    <Button variant="contained" color="default" style = {{backgroundColor: "#c43636", width: "90%", height: "2rem", fontSize: ".75rem"}}>
                         Remove Artist
                     </Button>
 				</div>
--- src/Scroll_list.js
+++ src/Scroll_list.js
@@ -27,25 +27,25 @@ export default function ArtistSongList() {
   var json = require('./DummyData/DummyArtists.json');
   const songs = json.graphicalPlaylists[0].artistNodes[3].tracks;
   return (
-  <div style = {{overflow: 'hidden', height: "60%", position: "relative"}}>
-    <List style = {{overflowY: "scroll", width:"100%", height: "100%", paddingRight: "3vw", boxSizing: "content-box"}}>
+  <div style = {{overflow: 'hidden', height: "calc(100% - 18rem)", position: "relative"}}>
+    <List style = {{overflowY: "scroll", width:"100%", height: "100%", paddingRight: "3rem", boxSizing: "content-box"}}>
       {songs.map(({spotifyTrackId, name, image, album}) => {
         const labelId = `checkbox-list-secondary-label-${spotifyTrackId}`;
         return (
-          <ListItem key={spotifyTrackId} button style={{marginTop: "1vh", marginBottom:"1vh", marginRight:"5vw", marginLeft:"1vw"}} >
+          <ListItem key={spotifyTrackId} button style={{marginLeft:"1rem"}} >
             <div>
-              <img alt="Album Cover" style = {{height: "2vw", width: "2vw", marginRight: "1vw"}} src={image}/>
+              <img alt="Album Cover" style = {{height: "2rem", width: "2rem", marginRight: "1rem"}} src={image}/>
             </div>    
             <ListItemText id={labelId} 
             disableTypography
-            primary={<Typography style={{ color: '#FFFFFF', fontSize: ".9vw" }}>{name}</Typography>}
-            secondary={<Typography style={{color: 'gray', fontSize: ".75vw" }}>{album}</Typography>}
+            primary={<Typography style={{ color: '#FFFFFF', fontSize: ".9rem" }}>{name}</Typography>}
+            secondary={<Typography style={{color: 'gray', fontSize: ".75rem" }}>{album}</Typography>}
             />
           
             <ListItemSecondaryAction>
               <Checkbox
-                icon={<FavoriteBorder style = {{width: "1.5vw", height: "1.5vw" }}/>}
-                checkedIcon={<Favorite style = {{width: "1.5vw", height: "1.5vw" }}/>}
+                icon={<FavoriteBorder style = {{width: "1.5rem", height: "1.5rem" }}/>}
+                checkedIcon={<Favorite style = {{width: "1.5rem", height: "1.5rem" }}/>}
                 edge="end"
                 onChange={handleToggle(spotifyTrackId)}
                 checked={checked.indexOf(spotifyTrackId) !== -1}
