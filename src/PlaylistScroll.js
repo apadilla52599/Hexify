2d8e45f196b582cd013f3de68947e7d5254fb1a1
mvcente <52298360+mvcente@users.noreply.github.com>
2020-12-14 16:55:35 -0500

    randomizer

--- src/PlaylistScroll.js
+++ src/PlaylistScroll.js
@@ -20,14 +20,14 @@ export default function Playlist(props){
                   <ListItemText 
                   disableTypography
                   primary={<div>
-                    <Typography style={{color: '#FFFFFF', fontSize: "70%"}}>{track.name.length > 40 ? (track.name.substring(0,40)+"...") : (track.name)}</Typography>
-                    <i className="fas fa-user-circle" style={{display:"inline-block", color: 'black', fontSize: "70%"}}></i>
-                    <Typography style={{display:"inline-block", color: '#EABFB9', fontSize: "70%"}}>{track.artist.name}</Typography>
+                    <Typography style={{color: '#FFFFFF', fontSize: "70%", whiteSpace: "nowrap"}}>{track.name.length > 27 ? (track.name.substring(0,27)+"...") : (track.name)}</Typography>
+                    {/* <i className="fas fa-user-circle" style={{display:"inline-block", color: 'black', fontSize: "70%"}}></i> */}
+                    <Typography style={{display:"inline-block", color: '#EABFB9', fontSize: "70%", whiteSpace: "nowrap"}}>{"🎤"+track.artist.name}</Typography>
                   </div>}
                   secondary={<div>
-                        <i className="fas fa-record-vinyl" style={{display:"inline-block", color: 'purple', fontSize: "8"}}></i>
-                        <Typography style={{display:"inline-block", color: 'gray', fontSize: "70%"}}>
-                          {track.album.name.length > 35 ? (track.album.name.substring(0,35)+"...") : (track.album.name)}
+                        {/* <i className="fas fa-record-vinyl" style={{display:"inline-block", color: 'purple', fontSize: "8"}}></i> */}
+                        <Typography style={{display:"inline-block", color: 'gray', fontSize: "70%", whiteSpace: "nowrap"}}>
+                          {track.album.name.length > 27 ? ("💿"+track.album.name.substring(0,27)+"...") : ("💿"+track.album.name)}
                         </Typography>
                   </div>}>
                     
